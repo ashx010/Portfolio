7f27d9c7b5ff46083563226e735c155621879d8d
ashx010 <yashaggarwal960@gmail.com>
2024-07-09 13:13:03 +0530

    add html, css, js

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+admin.site.site_header = 'Portfolio'
+admin.site.site_title = 'Scripet'
+admin.site.index_title = 'Scripet'
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('home.urls'))
 ]
